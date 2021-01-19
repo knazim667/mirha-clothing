9f5109d6596a2588a78b59c8460acc738fb02a99
knazim667 <nazamzahoor@yahoo.com>
2021-01-19 11:39:40 -0500

    destructure cart item function

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,21 +1,9 @@
 import ActionTypes from "../actions/types";
+import { addItemToCart } from "../redux/cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
   cartItems: [],
-};
-
-const addItemToCart = (cartItems, item) => {
-  const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
-
-  if (existingItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === item.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem
-    );
-  }
-  return [...cartItems, { ...item, quantity: 1 }];
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
