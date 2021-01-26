ef5e8d090e8074ef14e078801f86b82f177928c0
knazim667 <nazamzahoor@yahoo.com>
2021-01-26 12:05:31 -0500

    add click function to title of collections with router

--- src/components/collections/collections.js
+++ src/components/collections/collections.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./collections.css";
+import { withRouter } from "react-router-dom";
 import CollectionItems from "./collectionItems";
 
-const Collections = ({ title, items }) => {
+const Collections = ({ title, items, history, match, linkUrl }) => {
   return (
     <div className="collection-preview">
-      <h3 className="title">{title}</h3>
+      <h3
+        className="title"
+        onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}
+      >
+        {title}
+      </h3>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
@@ -17,4 +23,4 @@ const Collections = ({ title, items }) => {
   );
 };
 
-export default Collections;
+export default withRouter(Collections);
